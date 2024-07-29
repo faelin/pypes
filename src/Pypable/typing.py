05e5deb39768c17c9a96833483ebb82b02e9f944
Faelin Landy <faelin@umich.edu>
2024-07-29 13:13:23 -0400

    Update extend_class and add wrap_object

--- src/Pypable/typing.py
+++ src/Pypable/typing.py
@@ -60,24 +60,54 @@ def get_parent_class(__obj):
 		return __obj.__class__
 
 
-def extend_class(cls:type, *mixins:type, **attrs) -> type:
-	""" Shorthand method to extend an object of unknown type using a mixin. """
+def extend_class(cls:Union[type,str], *mixins:type, attrs:dict = None) -> type:
+	""" Dynamically extend a class with the specified mixins and attributes.
+
+	Parameters:
+		cls: The class to extend.
+		*mixins: Additional base-classes to include in the new class.
+			The `cls` class is appended to this list when generating the new subclass.
+		attrs: Additional attribute and method definitions for the class.
+			See the `Python docs <https://docs.python.org/3.9/library/functions.html#type>`_
+			for more information on the :py:class:`type` function.
+	"""
+
+	# classes currently available in the runtime
+	classes_in_frame = { name: obj for name, obj in inspect.getmembers(sys.modules[__name__]) if inspect.isclass(obj) }
+
+	if isinstance(cls, str): cls = classes_in_frame[cls]
 
 	if issubclass(cls, mixins): return cls  # return if already extended
 
+	# generate a name for the new class, to avoid collisions
 	mixin_names = [ mixin.__name__.title() for mixin in mixins ]
 	extension_name = ''.join(mixin_names) + cls.__name__.title()
-	classes_in_frame = { name: obj for name, obj in inspect.getmembers(sys.modules[__name__]) if inspect.isclass(obj) }
 
-	# generate the extension
+	# generate the extended class
 	if extension_name in classes_in_frame:
-		# the extended class already exists, attempt to instantiate it with cls
+		# the desired subclass already exists...
 		return classes_in_frame[extension_name]
 	else:
-		# otherwise we create the extended class, then attempt to instantiate it with cls
-		extended_class = type(extension_name, (*mixins, cls), attrs)
+		# otherwise we create the new subclass and return it
+		extended_class = type(extension_name, (*mixins, cls), attrs or {})
 		return extended_class
-	else:
+
+
+def wrap_object(__obj, *mixins:type, **attrs):
+	""" Cast the object such that it includes the specified mixins.
+
+	Parameters:
+		__obj: The object to wrap.
+		*mixins: Additional base-classes to include in the new class.
+			The `cls` class is appended to this list when generating the new subclass.
+		attrs: Additional attribute and method definitions for the class.
+			See the `Python docs <https://docs.python.org/3.9/library/functions.html#type>`_
+			for more information on the :py:class:`type` function.
+	"""
+
+	cls = get_parent_class(__obj)
+	return extend_class(cls, *mixins, **attrs)(__obj)
+
 
 
 # noinspection PyShadowingBuiltins
